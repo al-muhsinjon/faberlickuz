03c0c5bc0193ab541759dd3bdce92b0aaf2093f9
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2024-09-02 16:09:41 +0500

    Faberlic | Add Apbar Contact

--- src/components/layouts/responsively/app-bar.tsx
+++ src/components/layouts/responsively/app-bar.tsx
@@ -1,3 +1,145 @@
+// "use client";
+// import useCatalog from "@/hooks/use-catalog";
+// import useSearch from "@/hooks/use-search";
+// import { ISearchCategoryProduct } from "@/types";
+// import { Button, Input } from "@headlessui/react";
+// import { Contact, LayoutGrid, Phone, Search, X } from "lucide-react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import React, { useEffect, useState } from "react";
+// import { BsTelephone } from "react-icons/bs";
+
+// const AppBar = ({ locale }: { locale: string }) => {
+//   const { onOpen, onClose, isOpen } = useCatalog();
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const searchOpen = useSearch();
+
+//   const [opened, setOpened] = useState(false);
+//   const [product, setProduct] = useState<ISearchCategoryProduct>();
+
+//   const getSearch = async (params: string) => {
+//     if (!params) {
+//       // Clear results and close dropdown if input is empty
+//       setProduct(undefined);
+//       setOpened(false);
+//       return;
+//     }
+
+//     try {
+//       const res = await fetch(
+//         `${process.env.NEXT_API}/product-search?search=${params}`
+//       );
+//       const data = await res.json();
+//       setProduct(data);
+//       setOpened(true);
+//     } catch (error) {
+//       console.error("Failed to fetch search results:", error);
+//       setOpened(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getSearch(searchTerm);
+//   }, [searchTerm]);
+
+//   return (
+//     <>
+//       <div className="flex px-2 py-4 bg-white items-center border-b lg:hidden md:hidden justify-between">
+//         <Button
+//           onClick={() => {
+//             isOpen ? onClose() : onOpen();
+//           }}
+//           className={`border px-4 py-3 ml-7 ${
+//             isOpen
+//               ? "bg-main text-white hover:bg-main/90 ease-linear"
+//               : "border-main text-main font-semibold hover:text-main/90"
+//           }  rounded-lg flex gap-2`}
+//         >
+//           {isOpen ? <X /> : <LayoutGrid />}
+//         </Button>
+//         <Link href={"/"}>
+//           <div className="relative h-8 w-32">
+// <Image
+//   src={"/logo.jpg"}
+//   className="object-fill"
+//   fill
+//   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+//   alt="Logo"
+// />
+//           </div>
+//         </Link>
+//         <Button
+//           onClick={() => {
+//             searchOpen.isOpen ? searchOpen.onClose() : searchOpen.onOpen();
+//           }}
+//           className={`border px-4 py-3 ml-7 ${
+//             searchOpen.isOpen
+//               ? "bg-main text-white hover:bg-main/90 ease-linear"
+//               : "border-main text-main font-semibold hover:text-main/90"
+//           }  rounded-lg flex gap-2`}
+//         >
+//           {searchOpen.isOpen ? <X /> : <Search />}
+//         </Button>
+//       </div>
+//       {searchOpen.isOpen && (
+//         <>
+//           <div className="w-full px-6 flex">
+//             <Input
+//               type="search"
+//               placeholder="Search..."
+//               value={searchTerm}
+//               onChange={(e) => setSearchTerm(e.target.value)}
+//               className="w-full px-4 py-3 rounded-l-md outline-none bg-mainGray/15"
+//             />
+//           </div>
+//           {opened && (
+//             <div className="w-full mx-auto left-0 absolute  z-10 overflow-y-auto bg-white shadow-lg max-h-60 mt-1 rounded-md">
+//               {product && product.results && product.results.length > 0 ? (
+//                 product.results.map((prod) => (
+//                   <Link
+//                     href={`/uz/product/${prod.slug}`}
+//                     key={prod.id}
+//                     className="flex items-center p-2 hover:bg-gray-100 transition"
+//                   >
+//                     <div className="flex-shrink-0 h-16 w-16 relative mr-3">
+//                       <Image
+//                         priority={false}
+//                         src={prod.image.image}
+//                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+//                         alt={prod.image.image || "Product Image"}
+//                         fill
+//                         loading="lazy"
+//                         style={{ objectFit: "cover" }}
+//                         placeholder="blur"
+//                         blurDataURL="/blur-image.jpg"
+//                         className="w-full h-full object-cover duration-200 ease-in-out scale-100 blur-0 grayscale-0"
+//                       />
+//                     </div>
+//                     <div className="flex flex-col">
+//                       <span className="text-sm font-medium text-gray-900">
+//                         {prod.title_uz}
+//                       </span>
+//                       <span className="text-sm text-gray-500">
+//                         {prod.brand}
+//                       </span>
+//                       <span className="text-sm font-semibold text-gray-900">
+//                         ${prod.price}
+//                       </span>
+//                     </div>
+//                   </Link>
+//                 ))
+//               ) : (
+//                 <div className="p-4 text-gray-700">Hech nima topilmadi</div>
+//               )}
+//             </div>
+//           )}
+//         </>
+//       )}
+//     </>
+//   );
+// };
+
+// export default AppBar;
 "use client";
 import useCatalog from "@/hooks/use-catalog";
 import useSearch from "@/hooks/use-search";
@@ -7,7 +149,6 @@ import { Contact, LayoutGrid, Phone, Search, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { BsTelephone } from "react-icons/bs";
 
 const AppBar = ({ locale }: { locale: string }) => {
   const { onOpen, onClose, isOpen } = useCatalog();
@@ -44,18 +185,18 @@ const AppBar = ({ locale }: { locale: string }) => {
 
   return (
     <>
-      <div className="flex px-2 py-4 bg-white items-center border-b lg:hidden md:hidden justify-between">
+      <div className="flex px-4 py-3 bg-white items-center border-b lg:hidden md:hidden justify-between shadow-md">
         <Button
           onClick={() => {
             isOpen ? onClose() : onOpen();
           }}
-          className={`border px-4 py-3 ml-7 ${
+          className={`border px-4 py-2 rounded-lg ${
             isOpen
               ? "bg-main text-white hover:bg-main/90 ease-linear"
-              : "border-main text-main font-semibold hover:text-main/90"
-          }  rounded-lg flex gap-2`}
+              : "border-main text-main font-semibold hover:text-main/80"
+          } flex items-center gap-2`}
         >
-          {isOpen ? <X /> : <LayoutGrid />}
+          {isOpen ? <X size={20} /> : <LayoutGrid size={20} />}
         </Button>
         <Link href={"/"}>
           <div className="relative h-8 w-32">
@@ -68,36 +209,46 @@ const AppBar = ({ locale }: { locale: string }) => {
             />
           </div>
         </Link>
-        <Button
-          onClick={() => {
-            searchOpen.isOpen ? searchOpen.onClose() : searchOpen.onOpen();
-          }}
-          className={`border px-4 py-3 ml-7 ${
-            searchOpen.isOpen
-              ? "bg-main text-white hover:bg-main/90 ease-linear"
-              : "border-main text-main font-semibold hover:text-main/90"
-          }  rounded-lg flex gap-2`}
-        >
-          {searchOpen.isOpen ? <X /> : <Search />}
-        </Button>
+        <div className="flex items-center gap-2">
+          <Button
+            onClick={() => {
+              searchOpen.isOpen ? searchOpen.onClose() : searchOpen.onOpen();
+            }}
+            className={`border px-4 py-2 rounded-lg ${
+              searchOpen.isOpen
+                ? "bg-main text-white hover:bg-main/90 ease-linear"
+                : "border-main text-main font-semibold hover:text-main/80"
+            } flex items-center gap-2`}
+          >
+            {searchOpen.isOpen ? <X size={20} /> : <Search size={20} />}
+          </Button>
+          <Link href={`/${locale}/about`}>
+            <Button
+              className="border px-4 py-2 rounded-lg border-main text-main font-semibold hover:bg-main hover:text-white flex items-center gap-2"
+            >
+              <Contact size={20} />
+              <span className="hidden lg:block ml-2">Contact</span>
+            </Button>
+          </Link>
+        </div>
       </div>
       {searchOpen.isOpen && (
         <>
-          <div className="w-full px-6 flex">
+          <div className="w-full px-6 flex mt-2">
             <Input
               type="search"
               placeholder="Search..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full px-4 py-3 rounded-l-md outline-none bg-mainGray/15"
+              className="w-full px-4 py-2 rounded-md outline-none bg-mainGray/15"
             />
           </div>
           {opened && (
-            <div className="w-full mx-auto left-0 absolute  z-10 overflow-y-auto bg-white shadow-lg max-h-60 mt-1 rounded-md">
+            <div className="w-full mx-auto left-0 absolute z-10 overflow-y-auto bg-white shadow-lg max-h-60 mt-2 rounded-md">
               {product && product.results && product.results.length > 0 ? (
                 product.results.map((prod) => (
                   <Link
-                    href={`/uz/product/${prod.slug}`}
+                    href={`/${locale}/product/${prod.slug}`}
                     key={prod.id}
                     className="flex items-center p-2 hover:bg-gray-100 transition"
                   >
